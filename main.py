6c5ac05816a3d1d44ffcf9a5ccc4456efca09c5e
dibusure <dibusure@cock.li>
2023-12-02 16:32:06 +0300

    updated script

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#### TODO
+#### СОРТИРОВАТЬ ФАЙЛЫ ПО РАЗМЕРУ [X]
+#### СПИСОК ОТПРАВЛЕННЫХ ФАЙЛОВ [X] 
+
+
 import telebot
 from telebot import types
 import os
@@ -14,15 +19,29 @@ file = open('token.json')
 data = json.load(file)
 bot = telebot.TeleBot(data['token'])
 chat_id = '-1002137811828'
+maxfilesize = 50*2**20
 
-files =[]
+files = []
+filesnot = [] # files, that can't be sent 
+if os.path.exists('filessent.txt'):
+    with open('filessent.txt', 'r') as f:
+        filessent = eval(f.read()) # WARNING: remote execution
+else:
+    filessent = set()
 
 def listallfiles(startpath):
     for filename in os.listdir(startpath):
-        if os.path.isdir(startpath + "/" + filename):
-            listallfiles(startpath + "/" + filename)
+        fullname = startpath + "/" + filename
+        if os.path.isdir(fullname):
+            listallfiles(fullname)
         elif filename.lower().endswith(('.flac', '.mp3')):
-            files.append(startpath + "/" + filename)
+            if os.path.getsize(fullname) <= maxfilesize and fullname not in filessent:
+                files.append(fullname)
+            else:
+                filesnot.append(fullname)
+
+with open('filesnot.txt', 'w') as f:
+    f.write(str(filesnot))
 
 @bot.message_handler(commands=['start'])
 def send_files(message):
@@ -32,11 +51,18 @@ def send_files(message):
         try:
             bot.send_audio(chat_id=chat_id, audio=open(x, 'rb'))
         except telebot.apihelper.ApiTelegramException as e:
-            print(str(e))
-            sleeptime = int(str(e).split()[-1])
-            print("Sleeping", sleeptime)
-            time.sleep(sleeptime)
-            bot.send_audio(chat_id=chat_id, audio=open(x, 'rb'))
+            if int(str(e).split()[10].strip(".")) == 429:
+                print(str(e))
+                sleeptime = int(str(e).split()[-1])
+                print("Sleeping", sleeptime)
+                time.sleep(sleeptime)
+                bot.send_audio(chat_id=chat_id, audio=open(x, 'rb'))
+            else:
+                print(e)
+                with open('filessent.txt', 'w') as f:
+                    f.write(str(filessent))
+        finally:
+            filessent.add(x)
 
 
 bot.polling(none_stop=True, interval=0)
